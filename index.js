1a8daf3b96f29345ac626727de1d4bc28e84fb13
konan0802 <konanforbis@gmail.com>
2020-08-09 13:45:52 +0900

    add:コメントアウトの整理

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// グローバル変数
+// 以前通知したタスクの時間を保持
+let previousMinutes = "";
+
 const checkTime = function() {
     // 現時刻の取得
     var nowDate = new Date();
@@ -12,10 +16,8 @@ const checkTime = function() {
         nowMtoH = 0.5;
     }
     var nowTtoH = String(nowHours + nowMtoH);
-    console.log(nowHours+":"+nowMinutes);
-    console.log(nowTtoH);
 
-    // XMLHttpRequestオブジェクトの作成
+    // GASにGETリクエスト
     var request = new XMLHttpRequest();
     var URL = "https://script.google.com/macros/s/AKfycbwgabm961niluzXJkC-WEC05f__SxpqK_q0IeVyNI_yhnvLh6s/exec?time="+nowTtoH+"";
     request.open('GET', URL, true);
@@ -23,26 +25,34 @@ const checkTime = function() {
 
     // レスポンスが返ってきた時の処理を記述
     request.onload = function () {
+        // タスクの取り出し
         var arr = this.response;
-        console.log(arr);
+        var datetime = arr['datetime'].toString();
         var title = arr['task'].toString();
-        Push.create(title, {
-            body: '残り10分！！進捗はーーー？？',
-            timeout: 8000, // 通知が消えるタイミング
-            onClick: function() {
-            // 通知がクリックされた場合の設定
-            location.href='https://docs.google.com/spreadsheets/d/1lDxj0a44E8ASDH6hOVIXzfK8stUZXW2YVXqI206-djs/edit#gid=572962785';
-            }
-        });
+        // 以前通知したタスクの時間 ⇒ 既に通知されたタスクは通知しない
+        if(datetime!=previousMinutes){
+            // 通知を作成
+            Push.create(title, {
+                body: '残り10分！！進捗はーーー？？',
+                timeout: 8000, // 通知が消えるタイミング
+                onClick: function() {
+                // 通知がクリックされた場合の設定
+                location.href='https://docs.google.com/spreadsheets/d/1lDxj0a44E8ASDH6hOVIXzfK8stUZXW2YVXqI206-djs/edit#gid=572962785';
+                }
+            });
+            previousMinutes = datetime;
+        }
     }
-
+    //20分と50分の際にリクエスト送信
     if(nowMinutes == 20 || nowMinutes == 50){
         // リクエストをURLに送信
         request.send();
     }
-
 }
+//リロードされた際に
 window.onload = function() {
+    // 権限の確認
     Notification.requestPermission();
-    setInterval(checkTime, 60000);
+    // 30秒毎に実行
+    setInterval(checkTime, 30000);
 };
