1eb98ca5bcc12b9f368f2528efe19aa4ed756946
konan0802 <konanforbis@gmail.com>
2020-08-11 17:16:15 +0900

    ファビコンの設定

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php echo file_get_contents('index.html'); ?>
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>タスク通知App</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/push.js/0.0.11/push.min.js"></script>
+    <script src="index.js"></script>
+    <link rel="icon" href="./img/<?php echo strval(rand(1, 5)); ?>.ico">
+    <meta http-equiv="refresh" content="9000; URL=http://127.0.0.1:8080/">
+  </head>
+  <body>
+    <h1>タスク通知App</h1>
+  </body>
+</html>
