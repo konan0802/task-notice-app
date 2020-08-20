4272327f51e92a6cab595d7ee6663cedbe8a76f2
konan0802 <konanforbis@gmail.com>
2020-08-20 19:21:33 +0900

    fix:redirect URL

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/push.js/0.0.11/push.min.js"></script>
     <script src="index.js"></script>
     <link rel="icon" href="./img/<?php echo strval(rand(1, 5)); ?>.ico">
-    <meta http-equiv="refresh" content="9000; URL=http://127.0.0.1:8080/">
+    <meta http-equiv="refresh" content="9000; URL=https://task-notice-app.herokuapp.com/">
   </head>
   <body>
     <h1>タスク通知App</h1>
